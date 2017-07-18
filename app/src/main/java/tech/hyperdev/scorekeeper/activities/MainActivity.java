eb2ce4caaa9090c365e4332303a7feadde6dd06e
Jeremy <jeremychin24@gmail.com>
2017-07-18 23:20:52 +0800

    (6) Implement day and night theme modes

--- app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/activities/MainActivity.java
@@ -4,22 +4,60 @@ import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import tech.hyperdev.scorekeeper.R;
+import tech.hyperdev.scorekeeper.Utils;
 import tech.hyperdev.scorekeeper.fragments.ScoreFragment;
 
 public class MainActivity extends AppCompatActivity {
 
     private Fragment mFragment_1, mFragment_2;
+    private int mFlag = 0;
 
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.nightmode) {
+            if(mFlag==0) {
+                item.setTitle(R.string.day_mode);
+                Utils.changeToTheme(MainActivity.this, 1);
+                mFlag = 1;
+            }else{
+                item.setTitle(R.string.night_mode);
+                Utils.changeToTheme(MainActivity.this, 0);
+                mFlag = 0;
+            }
+
+
+
+//                Utils.changeToTheme(MainActivity.this, 0);
+//                item.setTitle(R.string.night_mode);
+        }
+
+
+        return true;
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        Utils.onActivityCreateSetTheme(this);
+
         setContentView(R.layout.activity_main);
-
-
 
 
         mFragment_1 = ScoreFragment.newInstance("Team 1");
@@ -28,9 +66,6 @@ public class MainActivity extends AppCompatActivity {
 
         FragmentManager fragmentManager = getSupportFragmentManager();
         fragmentManager.beginTransaction().replace(R.id.layout_fragment_1, mFragment_1).replace(R.id.layout_fragment_2, mFragment_2).commit();
-
-
-
 
 
     }
