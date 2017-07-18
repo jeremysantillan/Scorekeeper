1857194011dae0a21b28249731aad16ac1748e00
Jeremy <jeremychin24@gmail.com>
2017-07-18 18:20:53 +0800

    (4) Implement score counters

--- app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
+++ app/src/main/java/tech/hyperdev/scorekeeper/fragments/ScoreFragment.java
@@ -5,6 +5,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.ImageButton;
 import android.widget.TextView;
 
 import tech.hyperdev.scorekeeper.R;
@@ -16,9 +18,10 @@ import static android.R.attr.name;
  */
 public class ScoreFragment extends Fragment {
 
-    private TextView mTvTeamName;
+    private ImageButton mBtnPlus, mBtnMinus;
+    private TextView mTvTeamName, mTvCount;
     private String team_name;
-
+    private int counter = 0;
 
 
     public ScoreFragment() {
@@ -42,6 +45,30 @@ public class ScoreFragment extends Fragment {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_score, container, false);
         mTvTeamName = (TextView) view.findViewById(R.id.tvTeamName);
+        mTvCount = (TextView) view.findViewById(R.id.textView2);
+        mBtnMinus = (ImageButton) view.findViewById(R.id.btnMinus);
+        mBtnPlus = (ImageButton) view.findViewById(R.id.btnPlus);
+
+        mBtnMinus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(counter>0){
+                    counter--;
+                    mTvCount.setText(Integer.toString(counter));
+                }else{
+                    counter = 0;
+                    mTvCount.setText(Integer.toString(counter));
+                }
+            }
+        });
+
+        mBtnPlus.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                counter++;
+                mTvCount.setText(Integer.toString(counter));
+            }
+        });
 
         getBundle(getArguments());
         return view;
